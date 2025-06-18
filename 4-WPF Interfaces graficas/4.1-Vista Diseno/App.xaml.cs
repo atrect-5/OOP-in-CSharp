f948cd335bdc1d557145bacc4974266a02d8253b
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-06-18 16:09:15 -0600

    4.2-Vista Diseno

---
+++ 4-WPF Interfaces graficas/4.1-Vista Diseno/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _4._1_Vista_Diseno
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
