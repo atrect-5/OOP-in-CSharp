8fe42d9d4fd9b3090eafcc91f16a94a4b2acb954
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-06-18 21:54:01 -0600

    4.5-Eventos pt1

---
+++ 4-WPF Interfaces graficas/4.4-Eventos/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _4._4_Eventos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Validación de los números ingresados
+            if (!int.TryParse(txtNumero1.Text, out int numero1))
+            {
+                MessageBox.Show("El primer número no es válido.");
+                return;
+            }
+
+            if (!int.TryParse(txtNumero2.Text, out int numero2))
+            {
+                MessageBox.Show("El segundo número no es válido.");
+                return;
+            }
+
+            // Mostrar la suma de los números
+            MessageBox.Show($"La suma de {numero1} + {numero2} = {numero1 + numero2}");
+        }
+    }
+}
