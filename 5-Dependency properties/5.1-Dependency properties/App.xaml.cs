43f787de843dcaa3958f9dddb41bb477481e921e
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-06-21 00:51:16 -0600

    5.1-Dependency properties

---
+++ 5-Dependency properties/5.1-Dependency properties/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _5._1_Dependency_properties
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 5-Dependency properties/5.1-Dependency properties/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5._1_Dependency_properties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Declarando mi nueva dependency property
+
+        // Se define la propiedad 
+        public int MiPropiedad
+        {
+            get { return (int)GetValue(dependencyProperty); }
+            set { SetValue(dependencyProperty, value); }
+        }
+        // Se indican las especificaciones de mi propiedad (name, type, owner, metadata)
+        public static readonly DependencyProperty dependencyProperty =
+            DependencyProperty.Register("MiPropiedad", typeof(int), typeof (MainWindow), new PropertyMetadata(0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    // Nueva clase para crear una property
+    public class CustomButton : Button
+    {
+        public bool ChangeBackground
+        {
+            get { return (bool)GetValue(ChangeBackgroundProperty); }
+            set { SetValue(ChangeBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty ChangeBackgroundProperty =
+            DependencyProperty.Register("ChangeBackground", typeof(bool), typeof(CustomButton), new PropertyMetadata(false));
+
+    }
+}
